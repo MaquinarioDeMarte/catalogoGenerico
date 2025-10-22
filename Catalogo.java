5c41d68871e827a07661280f4529c1edda532da7
Newton Rück <96783305+MaquinarioDeMarte@users.noreply.github.com>
2025-10-22 10:52:50 -0300

    Finalização do projeto

--- Catalogo.java
+++ Catalogo.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Catalogo<T> {
     private List<T> lista = new ArrayList<>();
 
--- Principal.java
+++ Principal.java
@@ -2,6 +2,23 @@ public class Principal {
     Catalogo<String> catalogString = new Catalogo<>();
     Catalogo<Integer> catalogInteger = new Catalogo<>();
     public static void main(String[] args) throws Exception {
-        ///
+        Principal principal = new Principal();
+
+        principal.catalogString.adicionar("Matrix");
+        principal.catalogString.adicionar("O Poderoso Chefão");
+        principal.catalogString.adicionar("Clube da Luta");
+
+        principal.catalogInteger.adicionar(101);
+        principal.catalogInteger.adicionar(202);
+        principal.catalogInteger.adicionar(303);
+
+        System.out.println("=== Exibindo via método genérico ===");
+        System.out.println();
+        System.out.println("Catalogo de Filmes:");
+        Catalogo.exibirCatalogoGenerico(principal.catalogString);
+        System.out.println("-----------------------------");
+        System.out.println("Catalogo de Códigos:");
+        Catalogo.exibirCatalogoGenerico(principal.catalogInteger);
+        System.out.println("-----------------------------");
     }
 }
